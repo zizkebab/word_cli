b3e9c15566cb0555426d6b736ba91266e2c211bf
zionsofer <zion@windward.eu>
2018-10-29 19:00:14 +0200

    basic functionality of divide command

--- word_cli.py
+++ word_cli.py
@@ -4,11 +4,28 @@ import click
 def word_cli():
     pass
 
-# @word_cli.command()
-# @click.argument('f', type=click.Path(exists=True))
-# def divide():
-#     '''divide a file into to files: one with odds and one with evens'''
-#     click.echo('divide a file into to files: one with odds and one with evens')
+
+@word_cli.command()
+@click.argument('f', type=click.Path(exists=True))
+def divide(f):
+    '''divide a file into to files: one with odds and one with evens'''
+    click.echo("Dividing {0} into {0}_odd and {0}_even".format(f))
+    try:
+        output_odd = open(f + "_odd", "w")
+        output_even = open(f + "_even", "w")
+        with open(f, "r") as input:
+            for word in input.read().split():
+                if len(word) % 2 == 0:
+                    output_even.write(word + ", ")
+                else:
+                    output_odd.write(word + ", ")
+
+        output_odd.close()
+        output_even.close()
+    except IOError as e:
+        '''TODO: Log/Debug ...'''
+
+
 
 @word_cli.command()
 @click.argument('input', type=click.STRING, required=1)
